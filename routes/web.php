08c5eb82f9ea8fb88cee69707c9b16a2985f8fa0
mibra-heem <m.ibraheem4095332@gmail.com>
2024-10-19 23:42:26 +0500

    added new about route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('welcome');
+Route::get('/about', function () {
+    return view('about');
 });
